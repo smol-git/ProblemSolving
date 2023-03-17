8ecccf7bdc0243d73453f2d443c02622b3486e7a
Sohail <sohailahmedwork1@gmail.com>
2023-03-17 23:10:12 +0500

    updated

--- week 0 practice/Assets/Scripts/Palindrome.cs
+++ week 0 practice/Assets/Scripts/Palindrome.cs
@@ -4,31 +4,11 @@ using UnityEngine;
 
 public class Palindrome : MonoBehaviour
 {
-    int number = 234;
+    int number = 232;
     
     void Start()
     {
-        int reverse = 0;
-        int reminder = 0;
-        int tempNum = number;
-
-        if (number != 0)
-        {
-            reminder = number % 10;
-            reverse = reverse * 10 + reminder;
-            number /=  10;
-
-        }
-
-        if(reverse == tempNum)
-        {
-            Debug.Log("Number is palindrome" + number);
-        }
-        else
-        {
-            Debug.Log("Number is not palindrome" + number);
-        }
-        
+        CheckPalindrome();
     }
 
     // Update is called once per frame
@@ -36,5 +16,30 @@ public class Palindrome : MonoBehaviour
     {
         
 
+    }
+
+    void CheckPalindrome()
+    {
+        int reverse = 0;
+        int reminder = 0;
+        int tempNum = number;
+
+        while(number != 0)
+        {
+            reminder = number % 10;
+            reverse = reverse * 10 + reminder;
+            number /= 10;
+
+        }
+        Debug.Log(number);
+
+        if (reverse == tempNum)
+        {
+            Debug.Log("Number is palindrome" + reverse);
+        }
+        else
+        {
+            Debug.Log("Number is not palindrome" + reverse);
+        }
     }
 }
