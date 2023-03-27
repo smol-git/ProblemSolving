7ec7800a04e9b540e7f4b00353d677cd6165a027
Sohail <sohailahmedwork1@gmail.com>
2023-03-28 00:47:55 +0500

    updated

--- week 0 practice/Assets/Scripts/ComputerPlayer.cs
+++ week 0 practice/Assets/Scripts/ComputerPlayer.cs
@@ -4,27 +4,84 @@ using UnityEngine;
 
 public class ComputerPlayer : MonoBehaviour
 {
-    
+    Transform CurrentTransform;
+    public GameObject Ball;
+    public Rigidbody2D myRigidbody2D;
+    public float lerpSpeed = 10f;
     public float offset;
     public float speed;
     // Start is called before the first frame update
     void Start()
-    {  
-        transform.Translate(Vector2.up * 2);
+    {
+        myRigidbody2D.GetComponent<Rigidbody2D>();
+        CurrentTransform = this.transform;
+       // myRigidbody2D.AddForce(Vector2.up * speed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.y >= offset)
+        TestingAIPaddleWithLerp();
+        
+
+
+    }
+
+    void AIPaddleUpDownMovement()
+    {
+        if (transform.position.y >= offset)
         {
-            transform.Translate(Vector2.down * speed * Time.deltaTime);
-           
+            myRigidbody2D.AddForce(Vector2.down * speed);
         }
-        if(transform.position.y <= -offset)
+        if (transform.position.y <= -offset)
         {
-            transform.Translate(Vector2.up * speed * Time.deltaTime);
-            
+            myRigidbody2D.AddForce(Vector2.up * speed);
+        }
+    }
+
+    void TestingAIPaddleWithLerp()
+    {
+        if (Ball.transform.position.y > transform.position.y)
+        {
+            if (myRigidbody2D.velocity.y < 0)
+            {
+                myRigidbody2D.velocity = Vector2.zero;
+                myRigidbody2D.velocity = Vector2.Lerp(myRigidbody2D.velocity, Vector2.up * speed, lerpSpeed * Time.deltaTime);
+            }
+               
+        }
+        else if (Ball.transform.position.y < transform.position.y)
+        {
+            if (myRigidbody2D.velocity.y > 0)
+            {
+                myRigidbody2D.velocity = Vector2.zero;
+                myRigidbody2D.velocity = Vector2.Lerp(myRigidbody2D.velocity, Vector2.down * speed, lerpSpeed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            myRigidbody2D.velocity = Vector2.Lerp(myRigidbody2D.velocity, Vector2.zero * speed, lerpSpeed * Time.deltaTime);
+        }
+    }
+
+    void TestingAIPaddleWithoutLerp()
+    {
+        CurrentTransform = this.transform;
+
+        if (Ball.transform.position.x > 0 && Ball.transform.position.y > 0)
+        {
+            if (CurrentTransform.position.y < Ball.transform.position.y)
+            {
+                myRigidbody2D.velocity = new Vector2(0, 1) * speed;
+            }
+            else if (CurrentTransform.position.y > Ball.transform.position.y)
+            {
+                myRigidbody2D.velocity = new Vector2(0, -1) * speed;
+            }
+            else
+            {
+                myRigidbody2D.velocity = new Vector2(0, 0) * speed;
+            }
         }
     }
 }
